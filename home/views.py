8f4857d0d34161271f22c997cfe285fe66158d76
renanoliveira1221 <168653364+renanoliveira1221@users.noreply.github.com>
2025-05-28 17:06:20 -0300

    Create urls, templates and Update views from home

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    if request.method == "GET":
+        return render(request, "home.html")
+
+    elif request.method == "POST":
+        pass
